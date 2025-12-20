0a48add11bf00a8d12b6b9315bd118431a92d293
shergylchristie <zebariken@gmail.com>
2025-12-20 12:23:41 +0530

    fixing reading cart and wishlist length on login, and re-renders in nav

--- frontend_shopping/src/components/Navbar.jsx
+++ frontend_shopping/src/components/Navbar.jsx
@@ -26,8 +26,12 @@ export default function Navbar({ hydrated ,setActiveCategory }) {
   const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [showMobileSearch, setShowMobileSearch] = useState(false);
-  const cartCount = token && !hydrated ? null : useSelector((state) => state.cartItem.cart?.length || 0);
-  const wishlistCount = token && !hydrated ? null : useSelector((state) => state.wishlistItem.wishlist?.length || 0);
+  const cartLength =  useSelector((state) => state.cartItem.cart.length ?? 0);
+  const wishlistLength = useSelector((state) => state.wishlistItem.wishlist.length ?? 0);
+  const cartCount = token && !hydrated ? null : cartLength;
+  const wishlistCount = token && !hydrated ? null : wishlistLength;
+
+
 
   const dispatch = useDispatch();
 
