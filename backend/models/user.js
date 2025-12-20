0a231120ee28172bd7be9317080432e3ddc96904
shergylchristie <zebariken@gmail.com>
2025-12-20 13:34:05 +0530

    added phone field to user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,6 +5,7 @@ const regUser = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  phone: {type:Number, required:false},
   role: { type: String, enum: ["user", "admin"], default: "user" },
 });
 
