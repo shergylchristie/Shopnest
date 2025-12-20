cd24024a8b0aea11b9438ee1e2cad1099a83fcd8
shergylchristie <zebariken@gmail.com>
2025-12-21 02:08:55 +0530

    ....

--- frontend_shopping/src/components/OrderSuccess.jsx
+++ frontend_shopping/src/components/OrderSuccess.jsx
@@ -2,6 +2,7 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import { CheckCircle } from "lucide-react";
 import { resetPayment } from "../features/cartSlice";
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 
 
@@ -9,6 +10,7 @@ const OrderSuccess = () => {
   const { state } = useLocation();
   const navigate = useNavigate()
   const token = localStorage.getItem("token")
+  const dispatch = useDispatch()
 
   if (!state?.paymentId || !token) {
     return navigate("/login", { replace: true });
