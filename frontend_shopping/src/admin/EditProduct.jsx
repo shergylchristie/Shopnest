a362c89c6147aab08a2b1903ba402c347b055ac1
shergylchristie <zebariken@gmail.com>
2025-12-20 22:39:31 +0530

    .

--- frontend_shopping/src/admin/EditProduct.jsx
+++ frontend_shopping/src/admin/EditProduct.jsx
@@ -183,7 +183,7 @@ const EditProduct = () => {
             </label>
             <select
               name="category"
-              value={product.category}
+              value={form.category}
               onChange={handleChange}
               className="w-full border border-gray-300 rounded px-3 py-2"
             >
