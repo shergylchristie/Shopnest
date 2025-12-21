91338c72ac5db213337d9e03e9ac400f1cae9288
shergylchristie <zebariken@gmail.com>
2025-12-21 17:20:40 +0530

    >>>>

--- frontend_shopping/src/routes/RequireAuth.jsx
+++ frontend_shopping/src/routes/RequireAuth.jsx
@@ -1,6 +1,5 @@
 import { Navigate, useLocation } from "react-router-dom";
 
-
 const RequireAuth = ({ children }) => {
   const token = localStorage.getItem("token");
   const location = useLocation();
